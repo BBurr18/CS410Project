a274e71b4002566f487db970917a4ab6446df7f6
Alex Harris <alexharris@u.boisestate.edu>
2020-04-30 19:37:10 -0600

    Setup I think

---
+++ CS410 Project/src/main/java/edu/boisestate/cs410/gradebook/GradeBookShell.java
@@ -0,0 +1,30 @@
+package edu.boisestate.cs410.gradebook;
+
+import com.budhash.cliche.Command;
+import com.budhash.cliche.ShellFactory;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.*;
+
+public class GradeBookShell {
+    private final Connection db;
+
+    public GradeBookShell(Connection cxn) {
+        db = cxn;
+    }
+
+
+
+
+
+    public static void main(String[] args) throws IOException, SQLException {
+        // First (and only) command line argument: database URL
+        String dbUrl = args[0];
+        try (Connection cxn = DriverManager.getConnection("jdbc:" + dbUrl)) {
+            GradeBookShell shell = new GradeBookShell(cxn);
+            ShellFactory.createConsoleShell("grades", "", shell)
+                    .commandLoop();
+        }
+    }
+}
---
+++ CS410 Project/src/main/java/edu/boisestate/cs410/gradebook/GradeGenerator.java
@@ -0,0 +1,4 @@
+package edu.boisestate.cs410.gradebook;
+
+public class GradeGenerator {
+}
