49215d0779976aeb6fad1ffa69d22fb6a718549c
Alex Harris <alexharris@u.boisestate.edu>
2020-05-01 14:29:33 -0600

    I added select statements but I don't know if they are right.

--- CS410 Project/src/main/java/edu/boisestate/cs410/gradebook/GradeBookShell.java
+++ CS410 Project/src/main/java/edu/boisestate/cs410/gradebook/GradeBookShell.java
@@ -13,12 +13,12 @@ public class GradeBookShell {
     public GradeBookShell(Connection cxn) {
         db = cxn;
     }
-
+    // I am not sure if this is all he is looking for in the select
+    // Also I don't think the error handling is correct
     @Command
     public void select (String course_number) throws SQLException {
         String query =
-                "SELECT class_id, course_number,\n" +
-                        " term, section_number\n" +
+                "SELECT class_id, course_number\n" +
                         "FROM classes\n" +
                         "WHERE course_number = ?;";
         try (PreparedStatement stmt = db.prepareStatement(query)) {
@@ -28,7 +28,54 @@ public class GradeBookShell {
                     System.err.format("%d: class does not exist%n", course_number);
                     return;
                 }
-                System.out.format("%s %s %s %s%n",
+                System.out.format("%s %s%n",
+                        rs.getString("class_id"),
+                        rs.getString("course_number"));
+            }
+        }
+    }
+
+    @Command
+    public void select (String course_number, String term) throws SQLException {
+        String query =
+                "SELECT class_id, course_number, term\n" +
+                        "FROM classes\n" +
+                        "WHERE course_number = ?" +
+                        "and term = ?;";
+        try (PreparedStatement stmt = db.prepareStatement(query)) {
+            stmt.setString(1, course_number);
+            stmt.setString(2, term);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (!rs.next()) {
+                    System.err.format("%d: class does not exist%n", course_number);
+                    return;
+                }
+                System.out.format("%s %s %s %n",
+                        rs.getString("class_id"),
+                        rs.getString("course_number"),
+                        rs.getString("term"));
+            }
+        }
+    }
+
+    @Command
+    public void select (String course_number, String term, int section_number) throws SQLException {
+        String query =
+                "SELECT class_id, course_number, term, section_number\n" +
+                        "FROM classes\n" +
+                        "WHERE course_number = ?" +
+                        "and term = ?" +
+                        "and section_number = ?;";
+        try (PreparedStatement stmt = db.prepareStatement(query)) {
+            stmt.setString(1, course_number);
+            stmt.setString(2, term);
+            stmt.setInt(3, section_number);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (!rs.next()) {
+                    System.err.format("%d: class does not exist%n", course_number);
+                    return;
+                }
+                System.out.format("%s %s %s %s %n",
                         rs.getString("class_id"),
                         rs.getString("course_number"),
                         rs.getString("term"),
@@ -37,16 +84,6 @@ public class GradeBookShell {
         }
     }
 
-//    @Command
-//    public void echo(String... args) {
-//        for (int i = 0; i < args.length; i++) {
-//            if (i > 0) {
-//                System.out.print(' ');
-//            }
-//            System.out.print(args[i]);
-//        }
-//        System.out.println();
-//    }
 
     public static void main(String[] args) throws IOException, SQLException {
         // First (and only) command line argument: database URL
